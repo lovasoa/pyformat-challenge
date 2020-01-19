7cbe20f43e10677817a4588d0e91037ae6a02ac8
Ophir LOJKINE <pere.jobs@gmail.com>
2020-01-19 02:09:27 +0100

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 
-HACK_EVERYTHING = "" # TODO: find how to hack everything
-
-def extract_password_char(msg: str) -> str:
-   return "x" # TODO: find how to extract a password character
-
 password = ""
 
 for i in range(64):
     print(0)
-    print(HACK_EVERYTHING)
+    print("HACK_EVERYTHING") # TODO: find how to hack everything
     input()
     input()
-    password += extract_password_char(input())
+    password += extract_password_char(input()) # TODO: find how to extract a password character
     print(password)
     input()
